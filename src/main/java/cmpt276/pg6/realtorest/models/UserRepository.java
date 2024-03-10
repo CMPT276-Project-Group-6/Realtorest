24997005724d64159801a7a78564c6af132f6bf4
Htoor1999 <103155951+Htoor1999@users.noreply.github.com>
2024-03-10 12:52:35 -0700

    login database

---
+++ src/main/java/cmpt276/pg6/realtorest/models/UserRepository.java
@@ -0,0 +1,11 @@
+package cmpt276.pg6.realtorest.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    //List<User> findBySize(int size);
+
+    List<User> findByEmailAndPassword(String email, String password);
+}
