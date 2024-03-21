b9865092714d9382d73307a5fb89f7a9402745e9
kzcheng <kevinzifancheng@gmail.com>
2024-03-20 19:18:12 -0700

    Auto generated empty tests from source actions
    Yay! So many tests that passed!
    But yeah, before this gets merged into main, I'm gonna need to delete all the tests that aren't real.
    Otherwise there will be fake test coverage.

---
+++ src/test/java/cmpt276/pg6/realtorest/controllers/MainControllerTest.java
@@ -0,0 +1,75 @@
+package cmpt276.pg6.realtorest.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class MainControllerTest {
+    @Test
+    void testAddProperty() {
+
+    }
+
+    @Test
+    void testAddUser() {
+
+    }
+
+    @Test
+    void testDestroySession() {
+
+    }
+
+    @Test
+    void testGetCurrentUrl() {
+
+    }
+
+    @Test
+    void testLogin() {
+
+    }
+
+    @Test
+    void testReAdmin() {
+
+    }
+
+    @Test
+    void testReDev() {
+
+    }
+
+    @Test
+    void testReDevAdmin() {
+
+    }
+
+    @Test
+    void testShowDevPageProperties() {
+
+    }
+
+    @Test
+    void testShowDevPageUsers() {
+
+    }
+
+    @Test
+    void testShowHomePage() {
+
+    }
+
+    @Test
+    void testShowListingPage() {
+
+    }
+
+    @Test
+    void testShowLoginPage() {
+
+    }
+
+    @Test
+    void testShowRegisterPage() {
+
+    }
+}
