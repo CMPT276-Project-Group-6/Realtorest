1883fce8d2895e4a4c9c7df61605fbf2087ba79a
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-03-21 02:21:10 -0700

    init

--- src/main/resources/static/js/propertyListing.js
+++ src/main/resources/static/js/propertyListing.js
@@ -1,63 +1,64 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Getting data from database
-    // For now, just make up some data
-    const propertyData = [
-        {
-            id: 1,
-            price: 100,
-            bed: 4,
-            bath: 2,
-            area: 100,
-            address: "8888 University Dr, Burnaby, BC V5A 1S6",
-            images: ["Front.jpg", "Livingroom.jpeg", "Washroom.jpg"],
-        },
-        {
-            id: 2,
-            price: 150,
-            bed: 3,
-            bath: 2,
-            area: 102,
-            address: "13450 102 Ave #250, Surrey, BC V3T 0A3",
-            images: ["Front.jpg", "Livingroom.jpg", "Washroom.jpg"],
-        },
-        {
-            id: 3,
-            price: 650,
-            bed: 5,
-            bath: 4,
-            area: 259,
-            address: "515 W Hastings St, Vancouver, BC V6B 5K3",
-            images: ["Front.jpg", "Livingroom.jpeg", "Washroom.jpg"],
-        },
-        {
-            id: 4,
-            price: 650,
-            bed: 5,
-            bath: 4,
-            area: 259,
-            address: "800 Robson St, Vancouver, BC V6Z 2E7",
-            images: ["Front.jpg", "Livingroom.jpg", "Washroom.jpg"],
-        },
-        {
-            id: 5,
-            price: 650,
-            bed: 5,
-            bath: 4,
-            area: 259,
-            address: "2329 West Mall, Vancouver, BC Canada V6T 1Z4",
-            images: ["Front.jpg", "Livingroom.jpeg", "Washroom.jpg"],
-        },
-        {
-            id: 6,
-            price: 650,
-            bed: 5,
-            bath: 4,
-            area: 259,
-            address: "3333 University Way, Kelowna, BC Canada V1V 1V7",
-            images: ["Front.jpg", "Livingroom.jpg", "Washroom.jpg"],
-        },
-    ]
+// Getting data from database
+// For now, just make up some data
+const propertyData = [
+    {
+        id: 1,
+        price: 100,
+        bed: 4,
+        bath: 2,
+        area: 100,
+        address: "8888 University Dr, Burnaby, BC V5A 1S6",
+        images: ["Front.jpg", "Livingroom.jpeg", "Washroom.jpg"],
+    },
+    {
+        id: 2,
+        price: 150,
+        bed: 3,
+        bath: 2,
+        area: 102,
+        address: "13450 102 Ave #250, Surrey, BC V3T 0A3",
+        images: ["Front.jpg", "Livingroom.jpg", "Washroom.jpg"],
+    },
+    {
+        id: 3,
+        price: 650,
+        bed: 5,
+        bath: 4,
+        area: 259,
+        address: "515 W Hastings St, Vancouver, BC V6B 5K3",
+        images: ["Front.jpg", "Livingroom.jpeg", "Washroom.jpg"],
+    },
+    {
+        id: 4,
+        price: 650,
+        bed: 5,
+        bath: 4,
+        area: 259,
+        address: "800 Robson St, Vancouver, BC V6Z 2E7",
+        images: ["Front.jpg", "Livingroom.jpg", "Washroom.jpg"],
+    },
+    {
+        id: 5,
+        price: 650,
+        bed: 5,
+        bath: 4,
+        area: 259,
+        address: "2329 West Mall, Vancouver, BC Canada V6T 1Z4",
+        images: ["Front.jpg", "Livingroom.jpeg", "Washroom.jpg"],
+    },
+    {
+        id: 6,
+        price: 650,
+        bed: 5,
+        bath: 4,
+        area: 259,
+        address: "3333 University Way, Kelowna, BC Canada V1V 1V7",
+        images: ["Front.jpg", "Livingroom.jpg", "Washroom.jpg"],
+    },
+]
 
+// Load the property cards
+document.addEventListener("DOMContentLoaded", function () {
     // Get the container element in HTML
     const propertyContainer = document.getElementById("property-container")
 
@@ -74,6 +75,26 @@ function createPropertyCard(property) {
     const figure = document.createElement("figure")
     figure.className = "figure"
     figure.innerHTML = `
+        <!-- MODAL INFORMATION -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <!-- PROPERTY CARD -->
         <section id="carousel${property.id}" class="carousel slide">
             <section class="carousel-inner">
                 <section class="carousel-item active">
@@ -98,28 +119,31 @@ function createPropertyCard(property) {
                 <span class="visually-hidden">Next</span>
             </button>
         </section>
+
+        <!-- PROPERTY INFO -->
         <figcaption class="figure-caption">
-            <section class="icon-container">
-                <p class="property-price">$${property.price}K</h5>
-                <section class="icon-subcontainer">
-                    <i class="fa-solid fa-bed"></i>
-                    <p>${property.bed}</p>
+            <a href="">
+                <section class="icon-container">
+                    <p class="property-price">$${property.price}K</p>
+                    <section class="icon-subcontainer">
+                        <i class="fa-solid fa-bed"></i>
+                        <p>${property.bed}</p>
+                    </section>
+                    <section class="icon-subcontainer">
+                        <i class="fa-solid fa-bath"></i>
+                        <p>${property.bath}</p>
+                    </section>
+                    <section class="icon-subcontainer">
+                        <i class="fa-solid fa-house"></i>
+                        <p>1${property.area} ft&sup2;</p>
+                    </section>
                 </section>
-                <section class="icon-subcontainer">
-                    <i class="fa-solid fa-bath"></i>
-                    <p>${property.bath}</p>
+                <section class="address-container">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p class="property-address">${property.address}</p>
                 </section>
-                <section class="icon-subcontainer">
-                    <i class="fa-solid fa-house"></i>
-                    <p>1${property.area} ft&sup2</p>
-                </section>
-            </section>
-            <section class="address-container">
-                <i class="fa-solid fa-location-dot"></i>
-                <p class="property-address">${property.address}</p>
-            </section>
+            </a>
         </figcaption>
     `
-
     return figure
 }
