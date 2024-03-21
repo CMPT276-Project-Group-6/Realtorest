c0957342b5fb9fffacebc633d9bbe479d150e5cd
kzcheng <kevinzifancheng@gmail.com>
2024-03-20 18:22:12 -0700

    Refactor: Reordered setter and getters

--- src/main/java/cmpt276/pg6/realtorest/models/User.java
+++ src/main/java/cmpt276/pg6/realtorest/models/User.java
@@ -25,12 +25,12 @@ public class User {
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
+    public int getUid() {
+        return uid;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUid(int uid) {
+        this.uid = uid;
     }
 
     public String getUsername() {
@@ -41,19 +41,19 @@ public class User {
         this.username = username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getUid() {
-        return uid;
-    }
-
-    public void setUid(int uid) {
-        this.uid = uid;
     }
 }
