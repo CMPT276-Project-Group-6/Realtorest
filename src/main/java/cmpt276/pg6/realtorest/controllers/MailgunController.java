0861d06a4135f8e7848f4126728b823ce4957d0d
kzcheng <kevinzifancheng@gmail.com>
2024-04-02 06:29:47 -0700

    The API request can reach the Mailgun servers now!
    Also thanks god that I remembered to hide the API key this time lmao

--- src/main/java/cmpt276/pg6/realtorest/controllers/MailgunController.java
+++ src/main/java/cmpt276/pg6/realtorest/controllers/MailgunController.java
@@ -3,6 +3,7 @@ package cmpt276.pg6.realtorest.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,21 +14,29 @@ import kong.unirest.core.JsonNode;
 
 @Controller
 public class MailgunController {
+    /**
+     * Grabs the current URL and stores it as a model attribute, which means everything can use it. Mostly used for refreshing the page.
+     */
+    @ModelAttribute("currentUrl")
+    public String getCurrentUrl(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+
     @GetMapping("/dev/mail")
     public String showDevPageMail(Model model, HttpServletRequest request, HttpSession session) {
         return "dev/mail";
     }
 
-    /**
-     * 
-     */
     @PostMapping("/dev/mail/test")
     public String sendTestMail(@RequestParam String redirectUrl) {
-        HttpResponse<JsonNode> response = Unirest.post("http://localhost/post")
-            .header("accept", "application/json")
-            .queryString("apiKey", "123")
-            .field("parameter", "value")
-            .field("firstname", "Gary")
+        HttpResponse<JsonNode> response = Unirest
+            .post("https://api.mailgun.net/v3/REDACTED/messages")
+            .basicAuth("api", "REDACTED")
+            .field("from", "Mailgun Sandbox <postmaster@REDACTED.mailgun.org>")
+            .field("to", "CMPT276 Realtorest <cmpt276projectgroup6@gmail.com>")
+            .field("subject", "From Realtorest Application")
+            .field("text",
+                "This is sent from the Realtorest application. If you received this email, the Mailgun API is working correctly.")
             .asJson();
         return "redirect:" + redirectUrl;
     }
