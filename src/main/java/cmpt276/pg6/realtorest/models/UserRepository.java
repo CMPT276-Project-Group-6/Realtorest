21f42bc67a4ba9eecdb54c62d8cf3a104407133a
kzcheng <kevinzifancheng@gmail.com>
2024-04-04 09:12:40 -0700

    Added mailing list attribute to user database

--- src/main/java/cmpt276/pg6/realtorest/models/UserRepository.java
+++ src/main/java/cmpt276/pg6/realtorest/models/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByEmailAndPassword(String email, String password);
+
     List<User> findByEmail(String email);
 }
