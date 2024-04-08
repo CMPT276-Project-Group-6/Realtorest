8d8be0ca0934530934a8ee1b3fc30c7e729f37ee
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-04-08 16:38:43 -0700

    init

--- src/main/java/cmpt276/pg6/realtorest/controllers/PropertyController.java
+++ src/main/java/cmpt276/pg6/realtorest/controllers/PropertyController.java
@@ -55,7 +55,14 @@ public class PropertyController extends BaseController {
         addModelAttributeFromSession(session, model);
 
         Dotenv dotenv = Dotenv.configure().directory("./etc/secrets").load();
-        model.addAttribute("googleMapsApiKey", dotenv.get("GOOGLE_MAPS_API_KEY"));
+        String googleMapsApiKey = dotenv.get("GOOGLE_MAPS_API_KEY");
+        if (googleMapsApiKey == null) {
+            System.out.println("ERROR: GOOGLE_MAPS_API_KEY is null!");
+        } else {
+            System.out.println("GOOGLE_MAPS_API_KEY: " + googleMapsApiKey);
+        }
+        model.addAttribute("googleMapsApiKey", googleMapsApiKey);
+        
 
         Sort sort = Sort.by(sortOrder.equalsIgnoreCase("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC, "price");
 
