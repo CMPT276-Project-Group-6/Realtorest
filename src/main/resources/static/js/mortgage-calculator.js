424d0361db5691792f6931b291c0fbc635b918ab
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-04-09 13:38:23 -0700

    redesign the UI of mortgage calculator

--- src/main/resources/static/js/mortgage-calculator.js
+++ src/main/resources/static/js/mortgage-calculator.js
@@ -1,20 +1,17 @@
 let mortgage = 0;
-let payments = 0;
-let enterMort = document.getElementById('enterMortgage');
 
-
-
+// Calculates the user's mortgage
 function calculateMortgage(){
     mortgage = 0;
     let propertyPrice = Math.round(parseFloat(document.forms['mortgage']['price'].value) *100)/100; //rounds to 2 decimal place
     let downpayment = Math.round(parseFloat(document.forms['mortgage']['downpay'].value) *100)/100;
   
-    if(isNaN(downpayment) == true || isNaN(propertyPrice) == true){
-        alert("Please enter a valid property price and downpayment")
+    if(isNaN(downpayment) || isNaN(propertyPrice)){
+        alert("Please enter a valid property price and downpayment.")
     }
     else if(downpayment > propertyPrice){
         //todo: change alert to textbox near field
-        alert("Downpayment should not exceed property price");
+        alert("Down payment should not exceed property price.");
         
     }
     else{
@@ -23,36 +20,40 @@ function calculateMortgage(){
         showMortgage.innerHTML = '$' + mortgage.toFixed(2);
         calculatePayment();
     }
-}//Calculates the user's mortgage
+} 
 
 function calculatePayment(){
     let amortization = parseFloat(document.forms['payment']['amortization'].value);
     let frequency = document.forms['payment']['frequency'].value;
-    let ratePercent = Math.round(parseFloat(document.forms['payment']['rate'].value) *100)/100;
+    let ratePercent = Math.round(parseFloat(document.forms['mortgage']['rate'].value) *100)/100;
     let rate = ratePercent/100;//convert percent to decimal
     
     let showMortgage = document.getElementById("yourMortgage");
     let showPayments = document.getElementById("yourPayments");
 
-    if(showMortgage.innerHTML == ''){
+    if (showMortgage.innerHTML == ''){
         alert("Calculate your mortgage before you can find out your payments.")
     }//checks if there's a mortgage value
     else if(ratePercent > 50 || ratePercent < 0 || isNaN(ratePercent) == true){
-        alert("Enter a valid interest rate between %0.0 to %50.0.")
+        alert("Enter a valid interest rate between 0.0% to 50.0%.")
     }//checks if theres a valid interest rate value entered
     else{
         let interest = amortization*rate*mortgage;
         let totalPayment = mortgage+interest;
+
+        let resultSections = document.querySelectorAll('.result-section');
+        resultSections.forEach((section) => {
+            section.classList.remove('hidden');
+        });
         
-        if(frequency=='month'){
+        if (frequency=='month'){
             let payFrequency = amortization * 12; 
             let monthlyPayment = (Math.round((totalPayment/payFrequency) *100)/100).toFixed(2);
             showPayments.innerHTML = "$" + monthlyPayment + ' monthly'
-        }
-        else if(frequency =='biweek'){
+        } else if(frequency =='biweek'){
             let payFrequency = amortization * 24;
             let biweeklyPayment = (Math.round((totalPayment/payFrequency) *100)/100).toFixed(2);
             showPayments.innerHTML = "$" + biweeklyPayment + ' bi-weekly'
         }
-    }//calculate downpayments
+    } //calculate downpayments
 }
